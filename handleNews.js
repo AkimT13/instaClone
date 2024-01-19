9f7bfdac57ad55580a2034c3c516a0d58597dccb
AkimT13 <akimtarasov@gmail.com>
2024-01-19 15:25:45 -0800

    love how javascript things completly break after working fine for a day

--- handleNews.js
+++ handleNews.js
@@ -4,23 +4,26 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 import { ref, set } from 'firebase/database';
 import { database } from './src/Firebase/Firebase.js';
-import { process } from 'node';
+
+
 
 dotenv.config();
 
-export default async function handler(req, res) {
+export default async function handler() {
   try {
     //get data from newsapi
-    const apiKey = process.env.VITE_NEWSAPIKEY;
-    let response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`);
+    //const apiKey = process.env.VITE_NEWSAPIKEY;
+    const apiKey2 = "1200299230a54093ab0bd9a12413d913"
+    let response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey2}`);
     const headlines = response.data;
 
     // Store the data in the database
     await set(ref(database, "/headlines"), headlines);
 
-    res.status(200).json({ message: 'Data updated successfully' });
+    
   } catch (error) {
     console.error('Error updating data', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    
   }
 }
+handler();
